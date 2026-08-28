6bce004682deadb18284fb5b75ae00c7fe3a52d2
ton0967309761-a11y <ton0967309761@gmail.com>
2026-08-28 18:03:54 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE_NAME = '28-signs-pwa-v1';
-self.addEventListener('install', event => self.skipWaiting());
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', event => {});
